00aeaeb5990ecde9f59a8c2994d328937c573880
Abdul Qadir <157492762+Abdulqadir-1989@users.noreply.github.com>
2024-02-23 18:31:36 +0500

    Initial Work

---
+++ Class-1/app.js
@@ -0,0 +1,18 @@
+var address = "<center><h3>House # G 26/2, Malir Extension Collony Malir Karachi</h3></center>"
+var summary = "Presently I have been working in the Department of Community Health Sciences (CHS) as a Data Input Operator since January 2015. I have extensive experience of Data management, communication & coordination, data editing & discrepancy resolving and reporting. I am performing multiple tasks like Maintaining data sheet records, work on Redcap, work on WHO data software OpenClinica, update data error sheet on daily basis, make research study files, Data entry and editing, scanning all research related documents and data files, upload the study data on concern website and any other tasks as assigned by Manager.Presently I have been working in the Department of Community Health Sciences (CHS) as a Data Input Operator since January 2015. I have extensive experience of Data management, communication & coordination, data editing & discrepancy resolving and reporting. I am performing multiple tasks like Maintaining data sheet records, work on Redcap, work on WHO data software OpenClinica, update data error sheet on daily basis, make research study files, Data entry and editing, scanning all research related documents and data files, upload the study data on concern website and any other tasks as assigned by Manager."
+var qualification = "<ul><li>Bachelor of Arts final year from University of Karachi</li><li>Intermediate from board of Intermediate education</li><li>Matriculation from board of Secondary education</li></ul>"
+var experience = "<ul><li>Data Input OperatorAga Khan University January 2015-Present</li><li>Assistant Export Accountant Sohni Textile Industry January 2014 to November 2014</li><li>Assistant Purchaser Max Fashion Industries January 2011 to December 2013</li></ul>"
+var certificates = "<ul><li>Biomedical Responsible Conduct of Research from CITI Program</li><li>Certification Data Management, Analysis, and Computer Skill (Using SPSS) from Aga Khan University 2020</li></ul>"
+var personalinformation = "<table border=2px width=50%><tr><td>Father's Name</td><td>Abdul Rehman</td></tr><tr><td>Date of Birth</td><td>10-Oct-1989</td></tr><tr><td>Marital Status</td><td>Married</td></tr><tr><td>CNIC</td><td>42201-123456-7</td></tr><tr><td>Domicile</td><td>Karachi</td></tr></table>"
+
+
+document.write("<center><h1>Abdul Qadir</h1></center>")
+document.write(address)
+document.write("<center><h3>Email:rehmanabdulqadir@gmail.com</h3></center>")
+document.write('<hr>')
+document.write("<h2>Profile Summary\n</h2>" + summary)
+document.write("<h2>Academic Qualification \n</h2>" + qualification)
+document.write("<h2>Work Experience:</h2> \n" + experience)
+document.write("<h2>Certificate And Training:</h2> \n" + certificates)
+document.write("<h2>Personal Information</h2> \n" + personalinformation)
+document.write("<h2>Reference</h2> \n" + 'Will be provided upon request')
